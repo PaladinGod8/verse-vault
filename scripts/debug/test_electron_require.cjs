f53549bc3d406e4a9c3471ae9a5d9f5a9f3e0807
PaladinGod8 <230544824+PaladinGod8@users.noreply.github.com>
2026-02-26 22:52:19 +0800

    chore: moved script files from root to a dedicated /scripts/debug directory

---
+++ scripts/debug/test_electron_require.cjs
@@ -0,0 +1,14 @@
+'use strict';
+var electron = require('electron');
+var type = typeof electron;
+var out = 'type: ' + type + '\n';
+if (type === 'string') {
+  out += 'IS STRING (path): ' + electron + '\n';
+} else if (electron && type === 'object') {
+  out +=
+    'IS OBJECT keys: ' + Object.keys(electron).slice(0, 8).join(',') + '\n';
+}
+process.stdout.write(out);
+setTimeout(function () {
+  process.exit(0);
+}, 500);
